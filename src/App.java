9427c540f59ba63e17fe92dc9e76f411ee5c736c
marouanbougataya38-cmyk <marouanbougataya38@gmail.com>
2026-03-09 09:13:44 +0100

    Rename App class to Main

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import view.Consola;
 /**
  * Main entry point for the Library Management System.
  */
-public class App {
+public class Main {
     public static void main(String[] args) {
         Consola consola = new Consola();
 
